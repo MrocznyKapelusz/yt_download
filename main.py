2d6ef0f869193a148493c35dfd9690bed5fbe105
MrocznyKapelusz <kacper.starosciak@gmail.com>
2022-09-21 23:36:33 +0200

    Added progress Spinner with remaining KB to Download. Added simple-term-menu for resolution choosing. Added welcome banner.

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from pytube import YouTube
-import pytube
-import os
+from simple_term_menu import TerminalMenu
+from progress.spinner import Spinner
+from pyfiglet import Figlet
+
+# TODO: think of smarter way to show progress - maybe it is possible to use another bar
+# TODO: is it possible to merge DASH video and audio streams in Python? If so - implement :)
+
+def resolution_menu(streams):
+    options = [stream.resolution for stream in streams]
+    terminal_menu = TerminalMenu(options, title="Choose resolution")
+    menu_entry_index = terminal_menu.show()
+    print(f"You have selected {options[menu_entry_index]}!")
+    return options[menu_entry_index], menu_entry_index
 
 def main():
-    video_url = input('Enter YouTube video URL: ')
 
-    if os.name == 'nt':
-        path = os.getcwd() + '\\'
-    else:
-        path = os.getcwd() + '/'
+    f = Figlet(font='slant')
+    print(f.renderText('YT Downloader'))
 
-    name = pytube.extract.video_id(video_url)
+    bar = Spinner('KB left... ')
+    def progress_callback(*args):
+        bar.next()
+        bar.message = "KB left: " + str(args[2]/1000) + " "
 
-    YouTube(video_url).streams.first().download(filename=name)
-    yt = YouTube(video_url)
-    yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+    def complete_callback(*args):
+        bar.finish()
+        print("Download complete")
+
+    video_url = input('\nEnter YouTube video URL: ')
+
+    yt = YouTube(video_url, on_progress_callback=progress_callback, on_complete_callback=complete_callback)
+
+    print("\nQuerying for available resolution options...")
+
+    streams = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
+    chosen_res = resolution_menu(streams)
+    streams[chosen_res[1]].download()
 
 if __name__ == '__main__':
     main()
